ee4de3f210fea1ee596ccce71bda1e674239b128
REA5ON <valten007@gmail.com>
2023-11-17 15:23:23 +0100

    Initial

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+
+const form = useForm({
+    message: '',
+});
+</script>
+
+<template>
+    <Head title="Chirps" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            Viktor
+        </div>
+    </AuthenticatedLayout>
+</template>
